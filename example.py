80ef598e71a003b37bf809354f6088aa1b489743
cyhsieh0119 <96895088+cyhsieh0119@users.noreply.github.com>
2022-09-07 13:02:55 +0800

    Update example.py

--- example.py
+++ example.py
@@ -13,34 +13,34 @@ st.sidebar.markdown("# Main page 🎈 of Side Bar")
 
 
 def main():
-	    st.title("File Upload Tutorial")
-	    menu = ["Image","Dataset","DocumentFiles","About"]
-	    choice = st.sidebar.selectbox("Menu",menu)
+	st.title("File Upload Tutorial")
+	menu = ["Image","Dataset","DocumentFiles","About"]
+	choice = st.sidebar.selectbox("Menu",menu)
 
-	    if choice == "Image":
-			st.subheader("Image")
+	if choice == "Image":
+		st.subheader("Image")
 
-	    elif choice == "Dataset":
-			st.subheader("Dataset")
+	elif choice == "Dataset":
+		st.subheader("Dataset")
 
-	    elif choice == "DocumentFiles":
-			st.subheader("DocumentFiles")
+	elif choice == "DocumentFiles":
+		st.subheader("DocumentFiles")
 
 def load_image(image_file):
-	    img = Image.open(image_file)
-	    return img
+	img = Image.open(image_file)
+	return img
 
 if choice == "Image":
-	    st.subheader("Image")
-	    image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
+	st.subheader("Image")
+	image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
 
-	    if image_file is not None:
-			# To See details
-			file_details = {"filename":image_file.name, "filetype":image_file.type,
-					"filesize":image_file.size}
-			st.write(file_details)
-			# To View Uploaded Image
-			st.image(load_image(image_file),width=250)
+	if image_file is not None:
+		# To See details
+		file_details = {"filename":image_file.name, "filetype":image_file.type,
+				"filesize":image_file.size}
+		st.write(file_details)
+		# To View Uploaded Image
+		st.image(load_image(image_file),width=250)
 
 if __name__ == '__main__':
 	main()
