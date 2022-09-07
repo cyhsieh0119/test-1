cd21c3205dc397058f2ffdccc8f4c748d411bae7
cyhsieh0119 <96895088+cyhsieh0119@users.noreply.github.com>
2022-09-07 18:18:17 +0800

    Update example.py

--- example.py
+++ example.py
@@ -27,7 +27,7 @@ def main():
 			# To See details
 			file_details = {"filename":image_file.name, "filetype":image_file.type,
 					"filesize":image_file.size}
-			st.write(file_details)
+			st.sidebar.write(file_details)
 			# To View Uploaded Image
 			st.image(load_image(image_file),width=1000)
 
