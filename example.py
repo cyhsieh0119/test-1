a9339a14bee4911894343591d56e6aa7a8ca19b5
cyhsieh0119 <96895088+cyhsieh0119@users.noreply.github.com>
2022-09-07 09:29:54 +0800

    Update example.py

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-#import cv2
+from PIL import Image
 import streamlit as st
 import numpy as np
 import torch
@@ -10,3 +10,22 @@ st.set_page_config(page_title="Image converter" ,page_icon="random" ,layout="wid
 
 st.markdown("# Image converter page 🎈")
 st.sidebar.markdown("# Main page 🎈 of Side Bar")
+
+
+def load_image(image_file):
+	img = Image.open(image_file)
+	return img
+
+if choice == "Image":
+		st.subheader("Image")
+		image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
+
+		if image_file is not None:
+
+			  # To See details
+			  file_details = {"filename":image_file.name, "filetype":image_file.type,
+                              "filesize":image_file.size}
+			  st.write(file_details)
+
+              # To View Uploaded Image
+			  st.image(load_image(image_file),width=250)
