7c5bf56f4dc1f581708229e377eee7a7d17fc27e
cyhsieh0119 <96895088+cyhsieh0119@users.noreply.github.com>
2022-09-07 21:29:51 +0800

    Update example.py

--- example.py
+++ example.py
@@ -19,6 +19,11 @@ def main():
 	st.title("File Upload Tutorial")
 	menu = ["Image","Dataset","DocumentFiles","About"]
 	choice = st.sidebar.selectbox("Menu",menu)
+        tab1, tab2, tab3 = st.tabs(["img1", "Dog", "Owl"])
+
+#    with tab1:
+#        st.header("A cat")
+#        st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
 
 	if choice == "Image":
 		st.subheader("Image")
@@ -29,7 +34,9 @@ def main():
 					"filesize":image_file.size}
 			st.sidebar.write(file_details)
 			# To View Uploaded Image
-			st.image(load_image(image_file),width=1000)
+                        with tab1:
+                                st.header(image_file.name)
+			        st.image(load_image(image_file),width=1000)
 
 	elif choice == "Dataset":
 		st.subheader("Dataset")
