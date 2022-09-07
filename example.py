acc5b2e810827506ef5b5d3c91624b33041421f9
cyhsieh0119 <96895088+cyhsieh0119@users.noreply.github.com>
2022-09-07 09:20:18 +0800

    Update example.py

--- example.py
+++ example.py
@@ -6,8 +6,8 @@ import scipy.stats as st
 import plotly.express as px
 from matplotlib import pyplot as plt
 
-st.set_page_config(page_title="Image converter"
-                   ,page_icon="random"
+st.set_page_config(page_title="Image converter"\
+                   ,page_icon="random"\
                    ,layout="wide")
 
 st.markdown("# Image converter page 🎈")
