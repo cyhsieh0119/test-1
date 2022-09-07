dddc1e30437cf737d14784d35a0265fffc89a901
cyhsieh0119 <96895088+cyhsieh0119@users.noreply.github.com>
2022-09-07 18:07:07 +0800

    Update example.py

--- example.py
+++ example.py
@@ -19,6 +19,23 @@ def main():
 
 	if choice == "Image":
 		st.subheader("Image")
+		image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
+		if image_file is not None:
+			# To See details
+			file_details = {"filename":image_file.name, "filetype":image_file.type,
+					"filesize":image_file.size}
+			st.write(file_details)
+			# To View Uploaded Image
+			st.image(load_image(image_file),width=250)
+
+
+	if image_file is not None:
+		# To See details
+		file_details = {"filename":image_file.name, "filetype":image_file.type,
+				"filesize":image_file.size}
+		st.write(file_details)
+		# To View Uploaded Image
+		st.image(load_image(image_file),width=250)
 
 	elif choice == "Dataset":
 		st.subheader("Dataset")
@@ -30,17 +47,6 @@ def load_image(image_file):
 	img = Image.open(image_file)
 	return img
 
-if choice == "Image":
-	st.subheader("Image")
-	image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
-
-	if image_file is not None:
-		# To See details
-		file_details = {"filename":image_file.name, "filetype":image_file.type,
-				"filesize":image_file.size}
-		st.write(file_details)
-		# To View Uploaded Image
-		st.image(load_image(image_file),width=250)
 
 if __name__ == '__main__':
 	main()
