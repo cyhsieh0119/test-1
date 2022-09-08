195ca2bb005f6b240ecc52ee2fe2b8c952e0e3f3
cyhsieh0119 <96895088+cyhsieh0119@users.noreply.github.com>
2022-09-08 13:57:34 +0800

    Update example.py

--- example.py
+++ example.py
@@ -1,4 +1,7 @@
 from PIL import Image
+import cv2
+import numpy as np
+from PIL import Image
 import streamlit as st
 import numpy as np
 import torch
@@ -11,8 +14,63 @@ st.set_page_config(page_title="Image converter" ,page_icon="random" ,layout="wid
 st.markdown("# Image converter page 🎈")
 st.sidebar.markdown("# Main page 🎈 of Side Bar")
 
+def initD():
+	model_type = "DPT_Hybrid"   # MiDaS v3 - Hybrid   (medium accuracy, medium inference speed)
+
+	midas = torch.hub.load("intel-isl/MiDaS", model_type)
+
+	#
+	device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+	midas.to(device)
+	midas.eval()
+	#
+	midas_transforms = torch.hub.load("intel-isl/MiDaS", "transforms")
+
+	if model_type == "DPT_Large" or model_type == "DPT_Hybrid":
+		transform = midas_transforms.dpt_transform
+	else:
+		transform = midas_transforms.small_transform
+	
+def depthRwa(img0, ratio):
+	#img0 = cv2.imread(path)
+ 	#
+	img = cv2.cvtColor(img0, cv2.COLOR_BGR2RGB)
+	h, w = img.shape[0]*ratio, img.shape[1]*ratio
+	img = cv2.resize(img, (int(w), int(h)), 0, 0, interpolation=cv2.INTER_CUBIC)
+	#
+	input_batch = transform(img).to(device)
+	#
+	with torch.no_grad():
+		prediction = midas(input_batch)
+	#
+		prediction = torch.nn.functional.interpolate(
+				prediction.unsqueeze(1),
+				size=img.shape[:2],
+				mode="bicubic",
+				align_corners=False,
+				).squeeze()
+	#
+		output = prediction.cpu().numpy()
+    	return output
+
+	
+def depth(img):
+	cv_image = np.array(img) 
+	img = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
+
+	input_batch = transform(img).to(device)
+	with torch.no_grad():
+		prediction = midas(input_batch)
+		prediction = torch.nn.functional.interpolate(
+			prediction.unsqueeze(1),
+			size=img.shape[:2],
+			mode="bicubic",
+			align_corners=False,
+		).squeeze()
+
 def load_image(image_file):
-	img = Image.open(image_file)
+	img = cv2.imread(image_file)
+	#img = Image.open(image_file)
 	return img
 
 def main():
